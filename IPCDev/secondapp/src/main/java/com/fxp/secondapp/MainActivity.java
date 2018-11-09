c7afb741d42a333e7bcea7bc71af2b7b9dae580d
fangxiaopeng <850899969@qq.com>
2018-11-09 19:54:20 +0800

    update MainActivity.java

--- IPCDev/secondapp/src/main/java/com/fxp/secondapp/MainActivity.java
+++ IPCDev/secondapp/src/main/java/com/fxp/secondapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.fxp.secondapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBackService();
+    }
+
+    private void startBackService(){
+        try {
+            Intent serviceIntent = new Intent();
+            serviceIntent.setPackage("com.fxp.secondapp");
+            serviceIntent.setAction("com.fxp.secondapp.service.BackService");
+            startService(serviceIntent);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
